f2e4e390db3b0f841c4b76bb6f549014862b8ae4
shun989 <minhtuanvt89@gmail.com>
2021-05-20 14:22:58 +0700

    cau hinh laravel

--- myDemo/routes/web.php
+++ myDemo/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting/{name?}', function ($name = null) {
+    if ($name) {
+        echo 'Hello ' . $name . ' !';
+    }else {
+        echo 'Hello world !';
+    }
+});
